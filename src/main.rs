211628df157b267b60879bdbed2e2adb8ec8ced3
Otávio Pace <otaviopp8@gmail.com>
2021-07-27 01:17:19 -0300

    main: Add linker.instantiate

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@
 
 use anyhow::Result;
 use wasmtime::*;
+// use std::thread;
 
 fn main() -> Result<()> {
+    // let (result_sender, result_receiver): (Sender<Result<BlockState<C>, MappingError>>, _) = channel();
     // Configure the initial compilation environment, creating the global
     // `Store` structure. Note that you can also tweak configuration settings
     // with a `Config` and an `Engine` if desired.
@@ -21,10 +23,28 @@ fn main() -> Result<()> {
 
     let store = Store::new(&engine);
 
+    let mut linker = Linker::new(&store);
+    linker.func("host", "hello", || println!("hello"))?;
+
+    // let wat = r#"
+    // (module
+    //     (import "host" "double" (func $double (param i32) (result i32)))
+    //     (func (export "double") call $double)
+    // )
+    // "#;
+    let wat = r#"
+    (module
+      (func $hello (import "host" "hello"))
+      (func (export "run") (call $hello))
+    )
+    "#;
+    let module = Module::new(store.engine(), wat)?;
+    let instance = linker.instantiate(&module)?;
+
     // Compile the wasm binary into an in-memory instance of a `Module`.
     println!("Compiling module...");
     // WAT FILE
-    let module = Module::from_file(&engine, "wasm/hello.wat")?;
+    // let module = Module::from_file(&engine, "wasm/hello.wat")?;
 
     // WASM FILE
     // let binary = std::fs::read("wasm/hello.wasm")?;
@@ -33,24 +53,25 @@ fn main() -> Result<()> {
     // Here we handle the imports of the module, which in this case is our
     // `HelloCallback` type and its associated implementation of `Callback.
     println!("Creating callback...");
-    let hello_func = Func::wrap(&store, || {
-        println!("Calling back...");
-        println!("> Hello World!");
-    });
+    // let hello_func = Func::wrap(&store, || {
+    //     println!("Calling back...");
+    //     println!("> Hello World!");
+    // });
 
     // Once we've got that all set up we can then move to the instantiation
     // phase, pairing together a compiled module as well as a set of imports.
     // Note that this is where the wasm `start` function, if any, would run.
     println!("Instantiating module...");
-    let imports = [hello_func.into()];
-    let instance = Instance::new(&store, &module, &imports)?;
+    // let imports = [hello_func.into()];
+    // let instance = Instance::new(&store, &module, &imports)?;
 
     // Next we poke around a bit to extract the `run` function from the module.
     println!("Extracting export...");
-    let run = instance.get_typed_func::<(), ()>("run")?;
+    // let run = instance.get_typed_func::<i32, i32>("double")?;
+    // let result = run.call(12)?;
+    // println!("{}", result);
 
-    // And last but not least we can call it!
-    println!("Calling export...");
+    let run = instance.get_typed_func::<(), ()>("run")?;
     run.call(())?;
 
     println!("Done.");
