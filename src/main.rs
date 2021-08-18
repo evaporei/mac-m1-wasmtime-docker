33b33c74b541045e7febd4810ec1bfc2ce8a47b8
Otávio Pace <otaviopp8@gmail.com>
2021-08-18 08:47:01 -0300

    main: Clean up code and comments

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,22 @@
-//! Small example of how to instantiate a wasm module that imports one function,
-//! showing how you can fill in host functionality for a wasm module.
-
-// You can execute this example with `cargo run --example hello`
-
 use anyhow::Result;
-use wasmtime::*;
 use std::thread;
+use wasmtime::*;
 
 fn runtime() -> Result<()> {
-    // let (result_sender, result_receiver): (Sender<Result<BlockState<C>, MappingError>>, _) = channel();
-    // Configure the initial compilation environment, creating the global
-    // `Store` structure. Note that you can also tweak configuration settings
-    // with a `Config` and an `Engine` if desired.
     println!("Initializing...");
+
     let mut config = Config::new();
-    config.strategy(Strategy::Cranelift).unwrap();
-    config.interruptable(true); // For timeouts.
-    config.cranelift_nan_canonicalization(true); // For NaN determinism.
+    config.strategy(Strategy::Cranelift).unwrap(); // probably what causes the issue
+    config.interruptable(true);
+    config.cranelift_nan_canonicalization(true);
     config.cranelift_opt_level(OptLevel::None);
 
     let engine = Engine::new(&config)?;
-
     let store = Store::new(&engine);
 
     let mut linker = Linker::new(&store);
     linker.func("host", "hello", || println!("hello"))?;
 
-    // let wat = r#"
-    // (module
-    //     (import "host" "double" (func $double (param i32) (result i32)))
-    //     (func (export "double") call $double)
-    // )
-    // "#;
     let wat = r#"
       (module
         (func $hello (import "host" "hello"))
@@ -41,46 +26,23 @@ fn runtime() -> Result<()> {
     let module = Module::new(store.engine(), wat)?;
     let instance = linker.instantiate(&module)?;
 
-    // Compile the wasm binary into an in-memory instance of a `Module`.
+    // Useless logs just left because they were present
+    // in the failing example in README.md, but they
+    // have no relation to the issue.
     println!("Compiling module...");
-    // WAT FILE
-    // let module = Module::from_file(&engine, "wasm/hello.wat")?;
-
-    // WASM FILE
-    // let binary = std::fs::read("wasm/hello.wasm")?;
-    // let module = Module::from_binary(&engine, &binary)?;
-
-    // Here we handle the imports of the module, which in this case is our
-    // `HelloCallback` type and its associated implementation of `Callback.
     println!("Creating callback...");
-    // let hello_func = Func::wrap(&store, || {
-    //     println!("Calling back...");
-    //     println!("> Hello World!");
-    // });
-
-    // Once we've got that all set up we can then move to the instantiation
-    // phase, pairing together a compiled module as well as a set of imports.
-    // Note that this is where the wasm `start` function, if any, would run.
     println!("Instantiating module...");
-    // let imports = [hello_func.into()];
-    // let instance = Instance::new(&store, &module, &imports)?;
-
-    // Next we poke around a bit to extract the `run` function from the module.
     println!("Extracting export...");
-    // let run = instance.get_typed_func::<i32, i32>("double")?;
-    // let result = run.call(12)?;
-    // println!("{}", result);
 
     let run = instance.get_typed_func::<(), ()>("run")?;
     run.call(())?;
 
-    println!("Done.");
+    println!("Done."); // does not happen
     Ok(())
 }
 
 fn main() -> Result<()> {
-    let conf =
-        thread::Builder::new().name("cool thread name".into());
+    let conf = thread::Builder::new().name("cool thread name".into());
 
     let child: thread::JoinHandle<Result<()>> = conf.spawn(move || {
         runtime()?;
@@ -88,7 +50,7 @@ fn main() -> Result<()> {
     })?;
 
     let result = child.join();
-    println!("result: {:?}", result);
+    println!("result: {:?}", result); // does not happen
 
     Ok(())
 }
